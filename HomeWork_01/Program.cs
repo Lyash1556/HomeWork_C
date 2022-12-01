48de0d23c3732385d21aa5b74c44224a2b9ee921
Lyash1556 <Lyash1556@gmail.com>
2022-12-01 17:23:32 +0300

    the solution of the 4 task is in process

--- HomeWork_01/Program.cs
+++ HomeWork_01/Program.cs
@@ -1,13 +1,11 @@
 ﻿//Задача № 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-
+/*
 Console.WriteLine("Program for find max number between two numbers");
 
-Console.Write("input a first number: ");
-
+Console.Write("Input the first number: ");
 int a = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("input a second number: ");
-
+Console.Write("Input the second number: ");
 int b = Convert.ToInt32(Console.ReadLine());
 
 if(a > b)
@@ -17,4 +15,34 @@ if(a > b)
 else
 {
     Console.Write("The max number is " + b);
+}
+*/
+
+//Задача № 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Program for find max number between three numbers");
+
+int max;
+
+Console.Write("Input the first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+max = a;
+
+Console.Write("Input the second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("input the third number: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if(b > max)
+{
+    Console.Write("The max number is " + max);
+}
+if(c > max)
+{
+    Console.Write("The max number is " + c);
+}
+else
+{
+    Console.Write("The max number is " + a);
 }
