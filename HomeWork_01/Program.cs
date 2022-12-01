9937e196b4c67e21ed4d01a9846f7b37e55eae0c
Lyash1556 <Lyash1556@gmail.com>
2022-12-01 16:45:47 +0300

    added the solution of the 2 task

---
+++ HomeWork_01/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача № 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Program for find max number between two numbers");
+
+Console.Write("input a first number: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("input a second number: ");
+
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a > b)
+{
+    Console.Write("The max number is " + a);
+}
+else
+{
+    Console.Write("The max number is " + b);
+}
