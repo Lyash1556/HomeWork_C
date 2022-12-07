10d7b7b671734b6f352c594f01eb8fd77e13c712
Lyash1556 <Lyash1556@gmail.com>
2022-12-07 15:01:42 +0300

    add solutions for all tasks

--- HomeWork_02/Program.cs
+++ HomeWork_02/Program.cs
@@ -1,27 +1,29 @@
 ﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
+
 /*
-Console.WriteLine("Program for find the second number in a three-digit number");
+Console.WriteLine("The program for find the second number in a three-digit number.");
 
 int SecondNum(int number)
 {
-    int secnum = (number / 10) % 10;
-    return secnum;
+    int secNum = (number / 10) % 10;
+    return secNum;
 }
 
-Console.Write("Input number: ");
+Console.Write("Input a three-digit number: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
 int result = SecondNum(num);
 
-Console.WriteLine("The second number in your number is " + result);
+Console.WriteLine("The second number in your three-digit number is: " + result);
 */
 
 
 //Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-Console.WriteLine("The program for find the third number in a given number");
 
+/*
+Console.WriteLine("The program for find the third number in a given number.");
 
 int ThirdNum(int number)
 {
@@ -29,22 +31,44 @@ int ThirdNum(int number)
         {
             number = number / 10;
         }     
-        int findNum = number % 10;
+        int thirdNum = number % 10;
       
-        return findNum;
+        return thirdNum;
 }   
 
-Console.Write("input number: ");
+Console.Write("input an any number: ");
 int num = Convert.ToInt32(Console.ReadLine());
 if(num > 99)
 {
     int result = ThirdNum(num);
     Console.WriteLine(result);
 }
-else Console.WriteLine("Attention! The third nuber is not in your number");
+else Console.WriteLine("Attention! The third number is not in your number.");
+*/
 
 
 //Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
 
+/*
+Console.WriteLine("The program for find the weekend days.");
 
+bool FindDay(int n)
+{
+    if(n == 6 || n == 7) return true;
+    else return false;
+}
+
+Console.Write("Input tne day of week number: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if(day > 7)
+{
+    Console.WriteLine("It is not correct number!");
+}
+else
+{
+    bool result = FindDay(day);
+    Console.WriteLine(result);
+}
+*/
