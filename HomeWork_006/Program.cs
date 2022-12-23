64da31daf08a228f3bd7a31b42e028d6a87cd5f1
Lyash1556 <Lyash1556@gmail.com>
2022-12-23 09:51:45 +0300

    added the solutions for all tasks for HomeWork 6

--- HomeWork_006/Program.cs
+++ HomeWork_006/Program.cs
@@ -1,9 +1,5 @@
 ﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
-
 //1, -7, 567, 89, 223-> 3 - ошибка в проверочном примере №2.
-
-//желательно без массива.
-
 
 
 /*
@@ -31,36 +27,29 @@ Console.Write($"The amount of positive numbers is {result}.");
 
 
 //Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
-//
 //b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+/*
+Console.WriteLine("You are using the program for finding the point of intersection of straight lines by equations y = k1 * x + b1, y = k2 * x + b2.");
 
-//уравнения прямой линии
-//понять как найти точку пересечения зная коэффициенты
-
-//доп сложность
-//исключения если прямые параллельны.
-//две прямые погут совпадать
-
-void FindTochka(int b1, int k1, int b2, int k2)
+void FindTochka(double b1, double k1, double b2, double k2)
 {
     if(k1 != k2)
     {
-        int x = (b2 - b1)/(k1 - k2);
-        int y = k2 * x + b1;
-        Console.WriteLine($"точка пересечения на кооодинатах: {x} ; {y} .") 
+        double x = (b2 - b1)/(k1 - k2);
+        double y = ((b2 * k1) - (b1 * k2))/(k1 - k2);
+        Console.WriteLine($"The points of intersection of lines on coordinates: {x} ; {y} .");
     }           
-    else Console.WriteLine("Прямые параллельны одна другой");    
+    else Console.WriteLine("The straight lines are parallel");    
 }
 
-
-Console.Write("Введите число b1")
-bb1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число k1")
-kk1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число b2")
-bb2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число k2")
-kk2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input number b1: ");
+double bb1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input number k1: ");
+double kk1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input number b2: ");
+double bb2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input number k2: ");
+double kk2 = Convert.ToInt32(Console.ReadLine());
 
 FindTochka(bb1, kk1, bb2, kk2);
-
+*/
