4f6ff3c13a9d7cb3cf84ffe0496f5dd288b6a5fc
Lyash1556 <Lyash1556@gmail.com>
2022-12-28 15:37:15 +0300

    add solutions all tasks HomeWork of 7 seminar

--- HomeWork_007/Program.cs
+++ HomeWork_007/Program.cs
@@ -1,41 +1,4 @@
-﻿//болванки
-/*
-int[,] CreateRandom2dArray()
-{
-    Console.Write("Input a number of rows: ");
-    int rows = Convert.ToInt32(Console.ReadLine());
-    Console.Write("Input a number of columns: ");
-    int columns = Convert.ToInt32(Console.ReadLine());
-    Console.Write("Input a min passible value: ");
-    int minvalue = Convert.ToInt32(Console.ReadLine());
-    Console.Write("Input a max passible value: ");
-    int maxvalue = Convert.ToInt32(Console.ReadLine());
-    int [,] array = new int[rows, columns];
-    for (int i = 0; i < rows; i++)
-        for (int j = 0; j < columns; j++)
-            array[i,j] = new Random().Next(minvalue, maxvalue + 1);
-    return array;
-}
-void Show2dArray(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-            Console.Write(array[i,j] + "\t");
-        Console.WriteLine();
-    }
-    Console.WriteLine();
-
-*/
-
-
-
-
-
-
-
-//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-
+﻿//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 /*
 Console.WriteLine("This program creates a two-dimensional array of random real numbers.");
 
@@ -58,7 +21,7 @@ void Show2dArray(double[,] array)
     {
         for (int j = 0; j < array.GetLength(1); j++)
             Console.Write(array[i,j] + "\t");
-        Console.WriteLine();
+            Console.WriteLine();
     }
     Console.WriteLine();
 }
@@ -68,12 +31,10 @@ Show2dArray(myArray);
 */
 
 
-
-
 //Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
-
+//и возвращает значение этого элемента или же указание, что такого элемента нет. 
 /*
-Console.WriteLine("this program looks for the value of an element at a specific position.");
+Console.WriteLine("This program looks for the value of an element at a specific position.");
 
 int[,] CreateRandom2dArray()
 {
@@ -98,7 +59,7 @@ void Show2dArray(int[,] array)
     {
         for (int j = 0; j < array.GetLength(1); j++)
             Console.Write(array[i,j] + "\t");
-        Console.WriteLine();
+            Console.WriteLine();
     }
     Console.WriteLine();
 
@@ -132,66 +93,69 @@ FindElement(myArray, a, b);
 */
 
 
-
 //Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 /*
-Например, задан массив:
-1 4 7 2
-5 9 2 3
-8 4 2 4
-Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
-*/
-//вернуть 1 мерный массив.
+Console.WriteLine("This program looks arithmetic mean of the elements in each column of a two-dimensional array.");
 
-
-int[,] CreateRandom2dArray()
+double[,] CreateRandom2dArray()
 {
     Console.Write("Input a number of rows: ");
     int rows = Convert.ToInt32(Console.ReadLine());
     Console.Write("Input a number of columns: ");
     int columns = Convert.ToInt32(Console.ReadLine());
-    Console.Write("Input a min passible value: ");
-    int minvalue = Convert.ToInt32(Console.ReadLine());
-    Console.Write("Input a max passible value: ");
-    int maxvalue = Convert.ToInt32(Console.ReadLine());
-    int [,] array = new int[rows, columns];
+    double [,] array = new double[rows, columns];
     for (int i = 0; i < rows; i++)
         for (int j = 0; j < columns; j++)
-            array[i,j] = new Random().Next(minvalue, maxvalue + 1);
+            array[i,j] = new Random().NextDouble() * 10;
+    Console.WriteLine();
     return array;
 }
 
-void Show2dArray(int[,] array)
+void Show2dArray(double[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
-            Console.Write(array[i,j] + "\t");
+            Console.Write(Math.Round(array[i,j],2) + "\t");
         Console.WriteLine();
     }
     Console.WriteLine();
 }
 
-
-int[] FindAverage(int[,] array)
+double[] FindAverage(double[,] array)
 {
-    int average = 0;
-    int[] arr = new int[6]; 
-    
-        for (int i = 0; i < array.GetLength(1); i++)
+    double[] newArray = new double[array.GetLength(1)];
+    for (int j = 0; j < array.GetLength(1); j++)
+    {   
+        double sum = 0;
+        double avg = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
         {
-            for (int j = 0; j < array.GetLength(0); j++)
-            {
-                average = average + array[i, j];
-                arr[i] = average;                
-            }
+            sum = sum + array[i,j];
         }
-    return arr;
+        avg = sum / array.GetLength(0);
+        newArray[j] = Math.Round(avg, 1);
+    }
+    return newArray;
 }
 
+void ShowAvgArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == array.Length - 1)
+            Console.Write(array[i]);
+        else    
+            Console.Write(array[i] + ", ");
+    }
+    Console.WriteLine();
+}
 
-int [,] myArray = CreateRandom2dArray();
-
+double[,] myArray = CreateRandom2dArray();
 Show2dArray(myArray);
 
-FindAverage(myArray);
+Console.WriteLine("The array with arithmetic mean of the elements in each column is here: ");
+
+ShowAvgArray(FindAverage(myArray));
+Console.WriteLine();
+*/
