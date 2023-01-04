129b31415f4e0b148a21052f1d8791ae449dd61d
Lyashchenko A <Lyash1556@gmail.com>
2023-01-04 18:45:25 +0300

    add solution tasks seminar 8

--- HomeWork_008/Program.cs
+++ HomeWork_008/Program.cs
@@ -196,16 +196,62 @@ Show2dArray(result);
 
 //Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, 
 //которая будет построчно выводить массив, добавляя индексы каждого элемента.
+//
 /*
 Массив размером 2 x 2 x 2
 66(0,0,0) 25(0,1,0)
 34(1,0,0) 41(1,1,0)
 27(0,0,1) 90(0,1,1)
 26(1,0,1) 55(1,1,1)
+ */
+
+ int[, ,] CreateRandom3dArray()
+{
+    Console.Write("Input a number of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a number of columns: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a number of pages: ");
+    int pages = Convert.ToInt32(Console.ReadLine());
+    int minvalue = 10;
+    int maxvalue = 99;
+
+    int [, ,] array = new int[rows, columns, pages];
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            for (int k = 0; k < pages; k++)
+                array[i,j,k] = new Random().Next(minvalue, maxvalue + 1);
+                                    
+    return array;
+}
+
+void Show3dArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {    for (int j = 0; j < array.GetLength(1); j++)
+            for (int k = 0; k < array.GetLength(2); k++)
+            {     
+                Console.Write(array[i,j,k] + ($" ({i}, {j}, {k})") + "\t");
+            }
+
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
 
 
-*/
 
+int [,,] CorrectArray(int [,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            for (int k = 0; k < array.GetLength(2); k++)
+    return array;
+ 
+}
+
+int[,,] myArray = CreateRandom3dArray();
+Show3dArray(myArray);
 
 
 
